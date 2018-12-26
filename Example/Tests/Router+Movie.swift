01771825e5f84ff8d9ee5eb44d4191a8c811e0ca
Alexander Bozhko <alexander.bozhko@filmgrail.com>
2018-12-26 16:19:29 +0300

    first version

---
+++ Example/Tests/Router+Movie.swift
@@ -0,0 +1,9 @@
+//
+//  Router+Movie.swift
+//  KinoAppRouter_Tests
+//
+//  Created by Alexander Bozhko on 26/12/2018.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import Foundation
---
+++ Router+PageConfig.swift
@@ -0,0 +1,8 @@
+//
+//  Router+PageConfig.swift
+//  KinoAppRouter
+//
+//  Created by Alexander Bozhko on 26/12/2018.
+//
+
+import Foundation
